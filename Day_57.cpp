bd64d5ecb8288e6bd01ed959572d8ca7c3b38917
Shalu Patidar <136828396+shalupatidar22@users.noreply.github.com>
2024-09-07 14:38:11 +0530

    Find Nth Natural Number

---
+++ Day_57.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+long long findNth(long long n) {
+        // code here.
+       long long ans=0,base=1;
+       while(n){
+           ans+=base*(n%9);
+           base*=10;
+           n/=9;
+       }
+       return ans;
+    }
+
+int main(){
+    long long n;
+    cin>>n;
+    cout<<findNth(n);
+}
